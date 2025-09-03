326f6440329d37eb2c19618d55794a920d064b4e
oseembiya <ozeembiya@gmail.com>
2025-09-04 00:36:09 +0100

    ed

--- myProject/views.py
+++ myProject/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {'name': 'John'})
+
+
+def about(request):
+    return HttpResponse("About page")
